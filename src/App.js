5825c03aff4c616cd24543b7ee41a33ecad25d63
dormiebasne <dormiebasne@gmail.com>
2022-04-29 16:50:44 -0400

    Altered app.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -7,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Also, what the hell?!
+          Overtime Calculator
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
